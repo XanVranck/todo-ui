11685720aedd223e5e3cb042f96554fcbf191aa9
xanvranck <vranckaert.xan@gmail.com>
2018-11-15 17:25:27 +0100

    added navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
