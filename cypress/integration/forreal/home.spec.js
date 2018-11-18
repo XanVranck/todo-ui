6c8acae1bd7cab896a9183bbc048cc44182a42e4
xanvranck <vranckaert.xan@gmail.com>
2018-11-18 19:52:18 +0100

    messing around with cypress

--- cypress/integration/forreal/home.spec.js
+++ cypress/integration/forreal/home.spec.js
@@ -1,6 +1,6 @@
 describe('Home page', function() {
-    it('Visits the home page', function() {
-      cy.visit('localhost:4200')
+    it('Visits the home page and checks for image', function() {
+      cy.visit('/home')
 
       cy.get('img').should('be.visible');
     })
