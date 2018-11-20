64b10717f1d99ee6ee118ba52c522542398fb9ef
xanvranck <vranckaert.xan@gmail.com>
2018-11-20 21:29:14 +0100

    passed value from page to ts

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';   
+import {FirebaseProvider} from 'angular-firebase';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  constructor(private fb: FirebaseProvider) { }
+
 }
